ac8fb69d8525706605c103ca2330a950a331736c
Patrick Kimber <code@pkimber.net>
2013-12-04 19:51:36 +0000

    Update URLs for Django 1.4

--- mailgun_incoming/urls.py
+++ mailgun_incoming/urls.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import (
+    include,
+    patterns,
+    url,
+)
+
 from mailgun_incoming.views import Incoming
 
 urlpatterns = patterns('',
